9d65e5f76bd22c9bf8d64a39e9e73163fb7bd34f
Kodm1986 <kodm1986@gmail.com>
2023-02-11 23:38:19 +0300

    lessons 078-099

---
+++ lesson091.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    name: "Dmitry",
+    legs: 2,
+    tails: 1,
+    friends: ["Emma", "Alex"]
+  
+    // Only change code above this line
+  };
