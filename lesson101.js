33727ffe9053b08684cad8a46abddc8005847182
Kodm1986 <kodm1986@gmail.com>
2023-02-12 23:19:19 +0300

    lessons 100-104

---
+++ lesson101.js
@@ -0,0 +1,24 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+     {
+      "artist": "Michel Jackson",
+      "title": "Thriller",
+      "release_year": 1985,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
